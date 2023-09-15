5f7d1f19e95adbad5f4a7aa4956a2ba986bbaf29
Bond Vlad <wlad23072003@gmail.com>
2023-09-15 12:22:26 +0300

    fix_8.0

--- src/routes/Stack.tsx
+++ src/routes/Stack.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 // import Typed from 'react-typed';
 import styled from 'styled-components';
@@ -42,6 +43,35 @@ const Stack = () => {
           </div>
           <span>{`{`}</span>
         </Text> */}
+        <Text>
+          {/* <span>{`myStack: {`}</span> */}
+          <TypeAnimation
+            sequence={[
+              'myStack: {\n',
+              1000,
+              'myStack: {\nHTML ',
+              1000,
+              'myStack: {\nCSS ',
+              1000,
+              'myStack: {\nJavaScript ',
+              1000,
+              'myStack: {\nReact ',
+              1000,
+              'myStack: {\nRedux ',
+              1000,
+              'myStack: {\nTypeScript ',
+              1000,
+              'myStack: {\nTailwind ',
+              1000,
+              'myStack: {\nstyled-components ',
+            ]}
+            wrapper="span"
+            speed={10}
+            style={{ whiteSpace: 'pre-line', fontSize: '1em', display: 'inline-block' }}
+            repeat={Infinity}
+          />
+          <span>{`{`}</span>
+        </Text>
       </Wrapper>
     </>
   );
